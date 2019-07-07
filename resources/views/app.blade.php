8ed4bd5c436d74bb20ec11454fc9d5674d7d2eb6
Adryan Theo <adryantheo@gmail.com>
2019-07-08 03:43:03 +0700

    Init Axios

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>UMKMKu</title>
+        <link rel="stylesheet" sizes="192x192" href="{{ mix('/css/app.css') }}">
+    </head>
+    <body>
+        <div id="app">
+            <app-root></app-root>
+        </div>
+    </body>
+    <script src="{{ mix('/js/app.js') }}"></script>
+</html>

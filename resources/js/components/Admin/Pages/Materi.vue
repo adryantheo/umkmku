f24276d16a5c7ba52e77ef1cb2281d4a6c6d4ba4
A.Theo <adryantheo@gmail.com>
2019-10-13 22:20:30 +0700

    (FE) Materi's Tabs

--- resources/js/components/Admin/Pages/Materi.vue
+++ resources/js/components/Admin/Pages/Materi.vue
@@ -19,6 +19,83 @@
             >
                 {{item.name}}
             </v-tab>
+            <!-- pengenalan akutansi -->
+            <v-tab-item value="tab-0">
+                <v-layout row wrap>
+                    <v-card height="100%">
+                        <v-card-text>
+                            <p class="title font-weight-regular">Apa yang dimaksud dengan akuntansi?</p>
+                        </v-card-text>
+                        <v-card-text v-text="text"></v-card-text>
+                    </v-card>
+                </v-layout>
+            </v-tab-item>
+            <!-- elemen dan akun" -->
+            <v-tab-item value="tab-1">
+                <v-layout row wrap>
+                    <v-card height="100%">
+                        <v-card-text>
+                            <p class="title font-weight-regular">Laporan keuangan terdiri dari 6 elemen:</p>
+                        </v-card-text>
+                        <v-card-text v-text="text"></v-card-text>
+                    </v-card>
+                </v-layout>
+            </v-tab-item>
+            <!-- Kode Akun -->
+            <v-tab-item value="tab-2">
+                <v-layout row wrap>
+                    <v-card height="100%">
+                        <v-card-text>
+                            <p class="title font-weight-regular">Kode Akun:</p>
+                        </v-card-text>
+                        <v-card-text v-text="text"></v-card-text>
+                    </v-card>
+                </v-layout>
+            </v-tab-item>
+            <!-- Pencatatan Transaksi -->
+            <v-tab-item value="tab-3">
+                <v-layout row wrap>
+                    <v-card height="100%">
+                        <v-card-text>
+                            <p class="title font-weight-regular">Pencatatan Transaksi:</p>
+                        </v-card-text>
+                        <v-card-text v-text="text"></v-card-text>
+                    </v-card>
+                </v-layout>
+            </v-tab-item>
+            <!-- Penyesuiaan -->
+            <v-tab-item value="tab-4">
+                <v-layout row wrap>
+                    <v-card height="100%">
+                        <v-card-text>
+                            <p class="title font-weight-regular">Penyesuaian:</p>
+                        </v-card-text>
+                        <v-card-text v-text="text"></v-card-text>
+                    </v-card>
+                </v-layout>
+            </v-tab-item>
+            <!-- Manfaat" -->
+            <v-tab-item value="tab-5">
+                <v-layout row wrap>
+                    <v-card height="100%">
+                        <v-card-text>
+                            <p class="title font-weight-regular">Manfaat:</p>
+                        </v-card-text>
+                        <v-card-text v-text="text"></v-card-text>
+                    </v-card>
+                </v-layout>
+            </v-tab-item>
+            <!-- Jurnal Penutup -->
+            <v-tab-item value="tab-6">
+                <v-layout row wrap>
+                    <v-card height="100%">
+                        <v-card-text>
+                            <p class="title font-weight-regular">Jurnal Penutup:</p>
+                        </v-card-text>
+                        <v-card-text v-text="text"></v-card-text>
+                    </v-card>
+                </v-layout>
+            </v-tab-item>
     </v-tabs>
 </div>
 </template>
@@ -27,6 +104,7 @@
 export default {
     data: () => ({
         activeTab: null,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         tabItems: [
             {name: "Pengenalan Akutansi"},
             {name: "Elemen dan Akun-Akun Dalam Laporan Keuangan"},

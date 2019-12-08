86523816c76ffb975f41822c3c11835575ac06fc
A.Theo <adryantheo@gmail.com>
2019-12-09 00:48:47 +0700

    (FE) Added Penyesuaian's Contents

--- resources/js/components/Admin/Pages/Materi/penyesuaian.vue
+++ resources/js/components/Admin/Pages/Materi/penyesuaian.vue
@@ -1,16 +1,98 @@
 <template>
   <v-card height="100%">
         <v-card-text>
-            <p class="title font-weight-regular">Penyesuaian:</p>
+            <p class="title font-weight-regular-bold">Penyesuaian</p>
+            <p>Pencatatan penyesuaian dilakukan setiap akhir periode yang bertujuan untuk menyesuaikan peristiwa akuntansi dengan prinsip-prinsip akuntansi untuk mengoreksi kesalahan-kesalahan pencatatan ketika kesalahan itu baru diketahui di akhir periode. Akun yang dibuat jurnal penyesuaian antara lain:</p>
+            <p><b>1. Pendapatan yang Masih Akan Diterima</b></p>
+            <p>Pendapatan yang telah diterima menjadi hak perusahaan tetapi masih merupakan tagihan, sehingga akun pendapatan yang seharusnya bertambah dan akun kas bertambah, karena belum dibayarkan sehingga menjadi akun piutang (pendapatan yang masih akan diterima bertambah.</p>
+            <p><b>Contoh Transaksi:</b></p>
+            <p>Tanggal 31 Desember 2018, Perusahaan telah menyelesaikan pekerjaan berupa servis kendaraan senilai Rp5.000.000,00, akan tetapi pemilik kendaraan akan membayar jasa servis tersebut tanggal 5 Januari 2019.</p>
+            <p><b>Penyelesaian:</b></p>
+            <img src="/materi_img/penyesuaian_1.jpg" alt="">
         </v-card-text>
-        <v-card-text v-text="penyesuaian"></v-card-text>
+        <v-card-text>
+            <p><b>2. Beban yang Masih Harus Dibayar</b></p>
+            <p>Beban yang harus dibayarkan perusahaan kepada pihak lain atas jasa-jasa yang telah diberikan untuk kepentingan perusahaan. Beban tersebut belum dibayarkan,sehingga beban tersebut menjadi utang. Contoh akun beban yang masih harus dibayar( Utang beban) antara lain: Utang Gaji, Utang Listrik, Utang Air, Utang Pajak, Utang Bunga, dll.</p>
+            <p><b>Contoh Transaksi:</b></p>
+            <p>Tanggal 31 Desember 2018, Perusahaan mengakui gaji karyawan yang belum dibayarkan sebesar Rp500.000,00, akan dibayarkan perusahaan tanggal 3 Januari 2019.</p>
+            <p><b>Penyelesaian:</b></p>
+            <img src="/materi_img/penyesuaian_2.jpg" alt="">
+        </v-card-text>
+        <v-card-text>
+            <p><b>3. Beban Dibayar Dimuka</b></p>
+            <p>Beban-beban yang belum merupakan kewajiban perusahaan untuk membayar sesuai dengan periode yang bersangkutan, akan tetapi perusahaan sudah membayarnya terlebih dahulu. Contoh : Sewa Dibayar Dimuka, Asuransi Dibayar Dimuka. Ada dua cara pencatatan yaitu dengan pendekatan neraca dan pendekatan laba rugi.</p>
+            <p><b>Contoh Transaksi:</b></p>
+            <p>Tanggal 2 Desember 2018 perusahaan membayar sewa bangunan untuk satu tahun kedepan senilai Rp12.000.000,00 .</p>
+            <p><b>Pencatatan dengan pendekatan neraca</b></p>
+            <img src="/materi_img/penyesuaian_3.jpg" alt="">
+            <p>Tanggal 31 Desember 2018 merupakan saat penyusunan laporan keuangan perusahaan. Perusahaan sudah menggunakan bangunan tersebut selama satu bulan ( 2 desember- 31 desember) sehingga perusahaan mengakui beban sewa bangunan tersebut Rp1.000.000,00.</p>
+            <p>Dengan perhitungan Rp12.000.000,00/ 12 bulan x 1 bulan = Rp1.000.000,00</p>
+            <p><b>Jurnal Penyesuaian</b></p>
+            <img src="/materi_img/penyesuaian_3b.jpg" alt="">
+            <p><b>Pencatatan dengan pendekatan laba-rugi</b></p>
+            <img src="/materi_img/penyesuaian_3c.jpg" alt="">
+            <p>Tanggal 31 Desember 2018 merupakan saat penyusunan laporan keuangan perusahaan. Perusahaan sudah menggunakan bangunan tersebut selama satu bulan ( 2 desember- 31 desember) sehingga perusahaan mengakui beban sewa bangunan yang benar sudah dipakai Rp1.000.000,00 dan sisanya tersebut berupa aset perusahaan (sewa dibayar dimuka) Rp11.000.000,00.</p>
+            <p>Dengan perhitungan Rp12.000.000,00/ 12 bulan x 11 bulan = Rp11.000.000,00</p>
+            <p><b>Jurnal Penyesuaian</b></p>
+            <img src="/materi_img/penyesuaian_3d.jpg" alt="">
+        </v-card-text>
+        <v-card-text>
+            <p><b>4. Pendapatan Diterima Dimuka</b></p>
+            <p>Penerimaan dimuka berupa kas (uang tunai) atau barang untuk penyediaan barang dan jasa dimasa yang akan datang. Contoh: Belum melakukan pekerjaan tetapi sudah menerima uang (uang muka) berarti masih punya utang. Ada dua cara pencatatan yaitu dengan pendekatan neraca dan pendekatan laba rugi.</p>
+            <p><b>Contoh Transaksi:</b></p>
+            <p>Tanggal 2 Desember 2018, perusahaan menerima uang tunai dari Tuan Akbar sebagai pembayaran jasa yang akan dilakukan selama 3 bulan kedepan senilai Rp4.500.000,00</p>
+            <p><b>Pencatatan dengan pendekatan neraca</b></p>
+            <img src="/materi_img/penyesuaian_4.jpg" alt="">
+            <p>Namun, sampai tanggal 31 Desember 2018 (saat penyusunan laporan keuangan) perusahaan telah mengerjakan pekerjaan selama 1 bulan (2 desember-31 desember) dari total 3 bulan, sehingga perusahaan boleh mengakui pendapatan sebesar Rp1.500.000,00 (Rp4.500.000,00/3 bulan) x 1 bulan.</p>
+            <p><b>Jurnal Penyesuaian:</b></p>
+            <img src="/materi_img/penyesuaian_4b.jpg" alt="">
+            <p><b>Pencatatan dengan pendekatan laba-rugi</b></p>
+            <img src="/materi_img/penyesuaian_4c.jpg" alt="">
+            <p>Namun, sampai tanggal 31 Desember 2018 (saat penyusunan laporan keuangan) perusahaan telah mengerjakan pekerjaan selama 1 bulan (2 desember-31 desember) dari total 3 bulan, sehingga perusahaan boleh mengakui pendapatan sebesar Rp3.000.000,00 (Rp4.500.000,00/3 bulan) x 2 bulan.</p>
+            <p><b>Jurnal Penyesuaian:</b></p>
+            <img src="/materi_img/penyesuaian_4d.jpg" alt="">
+        </v-card-text>
+        <v-card-text>
+            <p><b>5. Beban Perlengkapan</b></p>
+            <p>Perlengkapan (bahan habis pakai) yang digunakan dalam perusahaan sebaiknya di catat secara periodik bisa mingguan,bulanan, ataupun tahunan. Pencatatan perlengkapan bisa dilakukan setiap hari tetapi jumlahnya yang relatif sedikit sangat tidak praktis jika dilakukan pencatatan setiap hari.</p>
+            <p><b>Contoh Transaksi:</b></p>
+            <p>Tanggal 1 Desember 2018, Perusahaan memiliki saldo perlengkapan sebesar Rp1.000.000,00.<br>Tanggal 31 Desember 2018, diketahui bahwa perlengkapan yang tersisa dalam perusahaan Rp400.000,00.</p>
+            <p><b>Penyelesaian:</b></p>
+            <p>Jumlah perlengkapan yang terpakai pada bulan desember adalah:<br><b>Saldo Awal + Pembelian – Saldo Akhir</b><br>Rp1.000.000,00 + Rp0,00 - Rp400.000,00 = Rp600.000,00 <br> Perlengkapan (Aset) yang digunakan akan berubah menjadi beban. Transaksi diatas menyebabkan beban perlengkapan bertambah di sisi debit sedangkan perlengkapan berkurang di sisi kredit. Jurnal transaksi tersebut adalah sebagai berikut:</p>
+            <img src="/materi_img/penyesuaian_5.jpg" alt="">
+        </v-card-text>
+        <v-card-text>
+            <p><b>6. Penyusutan Aset Tetap</b></p>
+            <p>Perusahaan yang menggunakan aset tetap untuk digunakan dalam kegiatan usaha, tentuhnya aset tersebut akan mengalami penyusutan nilainya dari hari-kehari semakin sedikit (kecuali tanah). Dalam akuntansi apabila aset jika digunakan atau dimanfaatkan dan bukan untuk dijual maka akan berubah menjadi beban, jika dijual maka tidak menjadi beban tetapi menjadi uang tunai (kas). Beban atas pengunaan aset tetap berwujud disebut dengan biaya penyusutan.</p>
+            <p><b>Contoh Transaksi:</b></p>
+            <p>Tanggal 31 Desember 2018, peralatan senilai Rp12.000.000,00 yang di beli pada 2 Desember 2018 ditaksirkan memiliki umur manfaat 5 tahun dengan nilai residu Rp6.000.000,00 
+                <br>Jurnal saat pembelian aset dapat dilihat pada jurnal umum bulan Desember 2018
+                <br>Cara menghitung besarnya penyusutan peralatan diatas yaitu dengan rumus:
+                <b>Penyusutan= (nilai perolehan – nilai residu)/ umur manfaat</b>
+                <br>Nilai perolehan : Nilai harga beli
+                <br>Nilai residu : Harga jual nilai akhir manfaat
+            </p>
+            <p><b>Penyelesaian:</b></p>
+            <p>Penyusutan peralatan
+                <br>=(Rp12.000.000,00 – Rp6.000.000,00)/ 5 tahun
+                <br>=Rp6.000.000,00/ 5 tahun
+                <br>=Rp1.200.000/ tahun
+                <br>Penyusutan bulan desember 2018  (2 desember sampai 31 desember) = 1 bulan
+                <br>Maka 1 bulan/ 12 bulan x Rp1.200.000,00= Rp100.000,00/ bulan
+                <br>Jurnal yang harus dibuat :
+            </p>
+            <img src="/materi_img/penyesuaian_6.jpg" alt="">
+            <v-flex text-xs-center>
+                <p><b>Daftar Saldo Setelah Penyesuaian “ Enggal Jaya”</b></p>
+                <img src="/materi_img/penyesuaian_6b.jpg" alt="">
+            </v-flex>
+        </v-card-text>
     </v-card>
 </template>
 
 <script>
 export default {
     data: () => ({
-        penyesuaian: 'penyesuaian'
     })
 }
 </script>
